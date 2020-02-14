ad7eecce28d7c16aa5beae0b1a544f54cdf06481
Luis Felipe Elgueta <luis.elgueta@trebol-it.cl>
2020-02-14 10:25:33 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { render } from '@testing-library/react';
+import Map from "./mapa";
+// import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// const [show, setShow] = useState(false);
+// const handleClose = () => setShow(false);
+// const handleShow = () => setShow(true);
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Map
+          googleMapURL= {'https://maps.googleapis.com/maps/api/js?key=AIzaSyDo2qy2mXS3GVnsczDX_ZAzRlZJnGK1Kzc'}
+          containerElement= {<div style={{height: '700px'}} />}
+          mapElement= {<div style={{height: '100%'}} />}
+          loadingElement= {<p>Cargando...</p>}
+        />
+
+      </div>
+    );
+
+
+    
+  }
+
+
+  
 }
 
+render (<App/>, document.getElementById('root'));
+
 export default App;
+
+
+
+
